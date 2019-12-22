14fae5d0065f5f4a65cfd723407ba31e2328bfac
Brian Clements <brian@clementscode.com>
2019-12-21 21:57:15 -0600

    advent day 5

---
+++ pkg/day5/incode_test.go
@@ -0,0 +1,140 @@
+package day5
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type slicesrc struct {
+	input []int
+	pos   int
+}
+
+func (src *slicesrc) read() (int, error) {
+	if src.pos > len(src.input)-1 {
+		return -1, fmt.Errorf("Ran out of input at %v", src.pos)
+	}
+	rval := src.input[src.pos]
+	src.pos++
+	return rval, nil
+}
+
+type slicedest struct {
+	output []int
+}
+
+func (dest *slicedest) write(i int) error {
+	dest.output = append(dest.output, i)
+	return nil
+}
+
+func TestParseInst(t *testing.T) {
+	data := []int{1002, 4, 3, 4, 33}
+	result, err := parseInstruction(0, data)
+	if err != nil {
+		t.Fatalf("Problem parsing input: %v", err)
+	}
+	if result.opcode != mult {
+		t.Errorf("Expected mult, got %v", result.opcode)
+	}
+	expectedModes := []int{0, 1, 0}
+	expectedArgs := []int{4, 3, 4}
+	if !reflect.DeepEqual(expectedModes, result.modes) {
+		t.Errorf("Expected modes %v, got %v", expectedModes, result.modes)
+	}
+	if !reflect.DeepEqual(expectedArgs, result.arguments) {
+		t.Errorf("Expected args %v, got %v", expectedArgs, result.arguments)
+	}
+}
+
+func TestInOut(t *testing.T) {
+	in := []int{42}
+	data := []int{3, 0, 4, 0, 99}
+	assertRunData(t, data, in, in)
+}
+
+func TestPosEq(t *testing.T) {
+	in := []int{8}
+	data := []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}
+	expected := []int{1}
+	assertRunData(t, data, in, expected)
+}
+
+func TestPosNEq(t *testing.T) {
+	data := []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}
+	in := []int{42}
+	expected := []int{0}
+	assertRunData(t, data, in, expected)
+}
+
+func TestPosLT(t *testing.T) {
+	data := []int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}
+	in := []int{7}
+	expected := []int{1}
+	assertRunData(t, data, in, expected)
+}
+
+func TestPosGTE(t *testing.T) {
+	data := []int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}
+	in := []int{9}
+	expected := []int{0}
+	assertRunData(t, data, in, expected)
+}
+
+func TestImmEq(t *testing.T) {
+	data := []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}
+	in := []int{8}
+	expected := []int{1}
+	assertRunData(t, data, in, expected)
+}
+
+func TestPJump(t *testing.T) {
+	data := []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}
+	in := []int{0}
+	expected := []int{0}
+	assertRunData(t, data, in, expected)
+}
+
+func TestPJumpNE(t *testing.T) {
+	data := []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}
+	in := []int{42}
+	expected := []int{1}
+	assertRunData(t, data, in, expected)
+}
+
+func TestRun1(t *testing.T) {
+	input := []int{1}
+	expected := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 13285749}
+	assertRunFullData(t, input, expected)
+}
+
+func TestRun5(t *testing.T) {
+	input := []int{5}
+	expected := []int{5000972}
+	assertRunFullData(t, input, expected)
+}
+
+func assertRunFullData(t *testing.T, input, expected []int) {
+	data, err := readData()
+	if err != nil {
+		t.Fatalf("Problem reading data: %v", err)
+	}
+	assertRunData(t, data, input, expected)
+}
+
+func assertRunData(t *testing.T, data, input, expected []int) {
+	in := &slicesrc{input, 0}
+	out := &slicedest{nil}
+	err := run(data, in, out)
+	if err != nil {
+		t.Fatalf("Problem running data: %v", err)
+	}
+	assertOutput(t, expected, out)
+}
+
+func assertOutput(t *testing.T, expected []int, out *slicedest) {
+	if !reflect.DeepEqual(expected, out.output) {
+		t.Fatalf("Expected output to be %v, but was %v", expected, out.output)
+	}
+}
---
+++ pkg/day5/intcode.go
@@ -0,0 +1,233 @@
+package day5
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+const (
+	add         = 1
+	mult        = 2
+	read        = 3
+	write       = 4
+	jumpIfTrue  = 5
+	jumpIfFalse = 6
+	lessThan    = 7
+	equals      = 8
+	quit        = 99
+)
+
+type instruction struct {
+	opcode    int
+	modes     []int
+	arguments []int
+}
+
+type intsrc interface {
+	read() (int, error)
+}
+
+type intdest interface {
+	write(i int) error
+}
+
+func run(data []int, in intsrc, out intdest) error {
+	ptr := 0
+	for ptr < len(data) {
+		inst, err := parseInstruction(ptr, data)
+		if err != nil {
+			return fmt.Errorf("Problem parsing at %v: %v", ptr, err)
+		}
+		if inst.opcode == quit {
+			return nil
+		}
+		var jumpDest int
+		jumpDest, err = execute(data, inst, in, out)
+		if err != nil {
+			return fmt.Errorf("Problem running at %v: %v", ptr, err)
+		}
+		if jumpDest > 0 {
+			ptr = jumpDest
+		} else {
+			ptr += len(inst.arguments) + 1
+		}
+	}
+	return fmt.Errorf("Ran out of data before termination")
+}
+
+func execute(data []int, inst *instruction, in intsrc, out intdest) (int, error) {
+	rval := 0
+	switch inst.opcode {
+	case add:
+		arg1 := getVal(0, inst, data)
+		arg2 := getVal(1, inst, data)
+		arg3 := inst.arguments[2]
+		fmt.Printf("%v + %v -> %v\n", arg1, arg2, arg3)
+		data[arg3] = arg1 + arg2
+	case mult:
+		arg1 := getVal(0, inst, data)
+		arg2 := getVal(1, inst, data)
+		arg3 := inst.arguments[2]
+		fmt.Printf("%v * %v -> %v\n", arg1, arg2, arg3)
+		data[arg3] = arg1 * arg2
+	case read:
+		i, err := in.read()
+		if err != nil {
+			return 0, err
+		}
+		index := inst.arguments[0]
+		fmt.Printf("read %v -> %v\n", i, index)
+		data[index] = i
+	case write:
+		arg := getVal(0, inst, data)
+		fmt.Printf("write %v\n", arg)
+		err := out.write(arg)
+		if err != nil {
+			return 0, err
+		}
+	case jumpIfTrue:
+		arg1 := getVal(0, inst, data)
+		arg2 := getVal(1, inst, data)
+		if arg1 != 0 {
+			fmt.Printf("%v != 0, jumping to %v\n", arg1, arg2)
+			rval = arg2
+		} else {
+			fmt.Printf("not jumping\n")
+		}
+	case jumpIfFalse:
+		arg1 := getVal(0, inst, data)
+		arg2 := getVal(1, inst, data)
+		if arg1 == 0 {
+			fmt.Printf("%v == 0, jumping to %v\n", arg1, arg2)
+			rval = arg2
+		} else {
+			fmt.Printf("not jumping\n")
+		}
+	case lessThan:
+		arg1 := getVal(0, inst, data)
+		arg2 := getVal(1, inst, data)
+		arg3 := inst.arguments[2]
+		if arg1 < arg2 {
+			fmt.Printf("%v < %v, storing 1 in %v\n", arg1, arg2, arg3)
+			data[arg3] = 1
+		} else {
+			fmt.Printf("%v >= %v, storing 0 in %v\n", arg1, arg2, arg3)
+			data[arg3] = 0
+		}
+	case equals:
+		arg1 := getVal(0, inst, data)
+		arg2 := getVal(1, inst, data)
+		arg3 := inst.arguments[2]
+		if arg1 == arg2 {
+			fmt.Printf("%v == %v, storing 1 in %v", arg1, arg2, arg3)
+			data[arg3] = 1
+		} else {
+			fmt.Printf("%v != %v, storing 0 in %v", arg1, arg2, arg3)
+			data[arg3] = 0
+		}
+	default:
+		return 0, fmt.Errorf("Unknown opcode: %v", inst.opcode)
+	}
+	return rval, nil
+}
+
+func getVal(argIndex int, inst *instruction, data []int) int {
+	arg := inst.arguments[argIndex]
+	mode := inst.modes[argIndex]
+	var rval int
+	if mode == 0 {
+		rval = data[arg]
+		fmt.Printf("get %v from %v\n", rval, arg)
+	} else {
+		rval = arg
+	}
+	return rval
+}
+
+func parseInstruction(ptr int, data []int) (*instruction, error) {
+	opStr := strconv.Itoa(data[ptr])
+	rval := instruction{0, nil, nil}
+	var err error
+	opLen := len(opStr)
+	var modeRunes []rune
+	if opLen > 2 {
+		index := opLen - 2
+		rval.opcode, err = strconv.Atoi(opStr[index:])
+		if err != nil {
+			return nil, err
+		}
+		if index > 0 {
+			modeStr := opStr[0:index]
+			modeRunes = []rune(modeStr)
+			for i, j := 0, len(modeStr)-1; i < j; i, j = i+1, j-1 {
+				modeRunes[i], modeRunes[j] = modeRunes[j], modeRunes[i]
+			}
+		}
+	} else {
+		rval.opcode = data[ptr]
+	}
+	var argCount int
+	switch rval.opcode {
+	case add:
+		argCount = 3
+	case mult:
+		argCount = 3
+	case read:
+		argCount = 1
+	case write:
+		argCount = 1
+	case quit:
+		argCount = 0
+	case jumpIfFalse:
+		argCount = 2
+	case jumpIfTrue:
+		argCount = 2
+	case lessThan:
+		argCount = 3
+	case equals:
+		argCount = 3
+	}
+	rval.modes, rval.arguments, err = parseArgs(ptr+1, data, argCount, modeRunes)
+	if err != nil {
+		return nil, err
+	}
+	return &rval, nil
+}
+
+func parseArgs(ptr int, data []int, argCount int, modeRunes []rune) (modes, args []int, err error) {
+	for i := 0; i < argCount; i++ {
+		var mode int
+		if i < len(modeRunes) {
+			mode, err = strconv.Atoi(string(modeRunes[i]))
+			if err != nil {
+				return
+			}
+			if mode < 0 || mode > 1 {
+				err = fmt.Errorf("Invalid mode %v: ", mode)
+				return
+			}
+		}
+		modes = append(modes, mode)
+		args = append(args, data[ptr+i])
+	}
+	return
+}
+
+func readData() ([]int, error) {
+	content, err := ioutil.ReadFile("../../resources/day5/input.txt")
+	if err != nil {
+		return nil, err
+	}
+	texts := strings.Split(string(content), ",")
+	rval := make([]int, len(texts))
+	for i := 0; i < len(texts); i++ {
+		num, err := strconv.Atoi(strings.TrimSpace(texts[i]))
+		if err != nil {
+			return nil, err
+		}
+		rval[i] = num
+	}
+	return rval, nil
+}
